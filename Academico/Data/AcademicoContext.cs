d6ea30f86877a2356b68f48a9fc655fad916511e
NESTOR CASTRO <2021101620@academicougb.com.br>
2023-04-04 20:56:37 -0300

    Configurada AcademicoContext com Instituição

---
+++ Academico/Data/AcademicoContext.cs
@@ -0,0 +1,14 @@
+﻿using Academico.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academico.Data
+{
+    public class AcademicoContext : DbContext
+    {
+        public AcademicoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<Instituicao> Instituicoes { get; set;}
+    }
+}
